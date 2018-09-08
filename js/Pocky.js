7a4d32c369dfbe71d1a09e6797a605f18d620eab
MakaoPoo <miyamasa0421@gmail.com>
2018-09-08 22:41:49 +0900

    チャット

--- js/Pocky.js
+++ js/Pocky.js
@@ -1,3 +1,2 @@
 $(window).on('load', function() {
-  initializePeer();
 });
--- js/WebRTC.js
+++ js/WebRTC.js
@@ -1,42 +1,61 @@
 var peer = null;
+var room = null;
 var conn = null;
 
-function initializePeer(callback) {
-  peer = new Peer({ key: '85a2adb9-746e-412b-b4ef-aca08e8ffcf0' });
-  peer.on('open', function(id) {
-    console.log('My peer ID is: ' + id);
-    $("#peer_id_label").text(id)
-    peer.on('connection', function(conn_data) {
-      conn = conn_data;
-      conn.on('open', function() {
-        // メッセージを受信
-        conn.on('data', function(data) {
-          $('#log').prepend("<p>" + data + "</p>");
-          console.log('Received', data);
-        });
-      });
-    });
-  });
-
-}
-
 $('#connect_btn').on('click', function() {
-  let input_id = $('#peer_id_input').val();
-  conn = peer.connect(input_id, {
-    metadata: {
-      hoge: "foobar",
-    }
-  });
-  conn.on('open', function() {
-
-    // メッセージを送信
-    conn.send('Hello!');
-  });
+  let peerId = $('#peer_id_input').val();
+  connectSkyway(peerId);
 });
 
-$('#send_msg_btn').on('click', function() {
-  if(conn != null) {
-    // メッセージを送信
-    conn.send($('#message_input').val());
+function connectSkyway(id) {
+  peer = new Peer(id ,{ key: '85a2adb9-746e-412b-b4ef-aca08e8ffcf0' });
+  peer.on('error', function(err) {
+    $('#log').prepend("<p>" + err + "</p>");
+    console.log('Received', err);
+  })
+  peer.on('open', function(data_id) {
+    console.log('My peer ID is: ' + data_id);
+    $("#peer_id_label").text(data_id)
+  });
+}
+
+$('#join_btn').on('click', function() {
+  let roomName = $('#room_name_input').val();
+
+  room = peer.joinRoom(roomName);
+  setupRoom(room);
+});
+$('#close_btn').on('click', function() {
+  if(room != null) {
+    room.close();
   }
+});
+
+function setupRoom(room) {
+  room.on('open', function() {
+    room.on('data', function(data) {
+      $('#log').prepend("<p>" + data.src + ": " + data.data + "</p>");
+    });
+    room.on('error', function(err) {
+      $('#log').prepend("<p>" + err + "</p>");
+      console.log('Received', err);
+    })
+    room.on('peerLeave', function(id) {
+      $('#log').prepend("<p>" + id + "が退室</p>");
+    })
+    // メッセージを送信
+    room.send('Hello!');
+  });
+}
+
+$('#send_msg_btn').on('click', function() {
+  if(room != null) {
+    // メッセージを送信
+    room.send($('#message_input').val());
+  }
+});
+
+$(window).on('unload', function() {
+  peer.disconnect();
+  peer.destroy();
 });
