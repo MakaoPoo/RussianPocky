4bcfb288ded58cf7cd154304877f8738a2102505
MakaoPoo <miyamasa0421@gmail.com>
2018-09-09 17:56:56 +0900

    チャットできた

--- js/Pocky.js
+++ js/Pocky.js
@@ -1,2 +1,59 @@
+var webRTC = new WebRTC();
+
 $(window).on('load', function() {
+
+});
+
+webRTC.on('message', function(data) {
+  $('#log').empty();
+  for(let msg of data) {
+    let myID = webRTC.getPeerID();
+    let msgP = $('<p>');
+    msgP.text("ID+"+msg.src+":"+msg.msg);
+    if(myID == msg.src) {
+      msgP.addClass("my_message");
+    }
+    let msgDiv = $('<div>').append(msgP);
+    $('#log').append(msgDiv);
+  }
+  $('#log_wrapper').scrollTop($('#log').height());
+});
+
+webRTC.on('open', function() {
+});
+
+webRTC.on('close', function() {
+  $('#log').empty();
+});
+
+webRTC.on('connect', function(peer) {
+  $('#peer_id_label').text(peer.id);
+});
+
+webRTC.on('destroy', function() {
+  $('#peer_id_label').text("ID:");
+});
+
+$('#connect_btn').on('click', function() {
+  let peerID = $('#peer_id_input').val();
+  webRTC.connectSkyway(peerID);
+});
+
+$('#join_btn').on('click', function() {
+  let roomName = $('#room_name_input').val();
+  webRTC.joinRoom(roomName, {
+
+  })
+});
+$('#close_btn').on('click', function() {
+  webRTC.closeRoom();
+});
+
+$('#send_msg_btn').on('click', function() {
+  let message = $('#message_input').val();
+  webRTC.sendData("msg", message);
+});
+
+$(window).on('unload', function() {
+  webRTC.destroyPeer();
 });
--- js/WebRTC.js
+++ js/WebRTC.js
@@ -1,61 +1,278 @@
-var peer = null;
-var room = null;
-var conn = null;
-
-$('#connect_btn').on('click', function() {
-  let peerId = $('#peer_id_input').val();
-  connectSkyway(peerId);
-});
-
-function connectSkyway(id) {
-  peer = new Peer(id ,{ key: '85a2adb9-746e-412b-b4ef-aca08e8ffcf0' });
-  peer.on('error', function(err) {
-    $('#log').prepend("<p>" + err + "</p>");
-    console.log('Received', err);
-  })
-  peer.on('open', function(data_id) {
-    console.log('My peer ID is: ' + data_id);
-    $("#peer_id_label").text(data_id)
-  });
-}
-
-$('#join_btn').on('click', function() {
-  let roomName = $('#room_name_input').val();
-
-  room = peer.joinRoom(roomName);
-  setupRoom(room);
-});
-$('#close_btn').on('click', function() {
-  if(room != null) {
-    room.close();
+class WebRTC {
+  constructor() {
+    this.peer = null;
+    // this.lobby = null;
+    // this.roomList = {};
+    this.room = null;
+    this.memberList = {};
+    this.messageList = [];
+    this.callback = {};
   }
-});
 
-function setupRoom(room) {
-  room.on('open', function() {
-    room.on('data', function(data) {
-      $('#log').prepend("<p>" + data.src + ": " + data.data + "</p>");
+  connectSkyway(id, callback = {}) {
+    if(this.peer != null) {
+      if(this.peer.id === id) {
+        console.log(id + "で既に接続しています");
+        return;
+      }else {
+        this.destroyPeer();
+      }
+    }
+    this.peer = new Peer(id,
+      { key: 'ec4d0c86-ae0b-4313-b813-2b0511a60a42' }
+    );
+
+    let webRTC = this;
+    this.peer.on('open', function(data_id) {
+      console.log("Skywayに接続");
+      console.log('あなたのID: ' + data_id);
+
+      webRTC.trigger("connect", this);
     });
-    room.on('error', function(err) {
-      $('#log').prepend("<p>" + err + "</p>");
-      console.log('Received', err);
-    })
-    room.on('peerLeave', function(id) {
-      $('#log').prepend("<p>" + id + "が退室</p>");
-    })
-    // メッセージを送信
-    room.send('Hello!');
-  });
-}
+    this.peer.on('error', function(err) {
+      switch(err.type) {
+        case "invalid-key":
+        alert("APIキーが不正です");
+        console.log("APIキーが不正です");
+        break;
 
-$('#send_msg_btn').on('click', function() {
-  if(room != null) {
-    // メッセージを送信
-    room.send($('#message_input').val());
+        case "room-error":
+        alert("ルームエラーです");
+        console.log("ルームエラーです");
+        break;
+
+        case "unavailable-id":
+        alert("IDが不正です");
+        console.log("IDが不正です");
+        break;
+
+        case "peer-unavailable":
+        alert("Peerが不正です");
+        console.log("Peerが不正です");
+        break;
+
+        default:
+        console.log(err.type);
+      }
+      webRTC.trigger("error", err.type);
+      webRTC.destroyPeer();
+    });
   }
-});
+  //
+  // joinLobby(callback = {}) {
+  //   if(peer == null || peer.isDisconnected) {
+  //     console.log("Peer is not connected Skyway");
+  //     return;
+  //   }
+  //   this.lobby = this.peer.joinRoom("MakaoPooGamesLobby");
+  //   this.lobby.on('open', function() {
+  //     console.log("Join lobby");
+  //     if(typeof(callback["open"]) == "function") {
+  //       callback["open"](data);
+  //     }
+  //     this.lobby.on('data', function(data) {
+  //       console.log(data);
+  //       if(typeof(callback["data"]) == "function") {
+  //         callback["data"](data);
+  //       }
+  //     });
+  //     this.lobby.on('error', function(err) {
+  //       console.log(err);
+  //       if(typeof(callback["error"]) == "function") {
+  //         callback["error"](err);
+  //       }
+  //     })
+  //     this.lobby.on('peerJoin', function(id) {
+  //       console.log(id + " join");
+  //       if(typeof(callback["join"]) == "function") {
+  //         callback["join"](id);
+  //       }
+  //     })
+  //     this.lobby.on('peerLeave', function(id) {
+  //       console.log(id + " leave");
+  //       if(typeof(callback["leave"]) == "function") {
+  //         callback["leave"](id);
+  //       }
+  //     })
+  //   });
+  // }
 
-$(window).on('unload', function() {
-  peer.disconnect();
-  peer.destroy();
-});
+  joinRoom(roomName) {
+    if(this.peer == null || this.peer.isDisconnected) {
+      console.log("Skywayに接続されていません");
+      return;
+    }
+    if(roomName == "") {
+      console.log("ルーム名が入力されていません");
+      return;
+    }
+    if(this.room != null) {
+      if(this.room.name === roomName) {
+        console.log(roomName + "には既に入室しています");
+        return;
+      }else {
+        this.closeRoom();
+      }
+    }
+    this.room = this.peer.joinRoom(roomName);
+
+    let webRTC = this;
+    this.room.on('open', function() {
+      webRTC.openRoom();
+      this.on('data', function(data) {
+        webRTC.categorizeData(data);
+      });
+      this.on('peerJoin', function(id) {
+        webRTC.addMember(id);
+      })
+      this.on('peerLeave', function(id) {
+        webRTC.deleteMember(id);
+      })
+      this.on('log', function(logList) {
+        for(let logJson of logList) {
+          let log = JSON.parse(logJson);
+          switch(log.messageType) {
+            case "ROOM_USER_JOIN":
+            webRTC.addMember(log.message.src)
+            break;
+
+            case "ROOM_USER_LEAVE":
+            webRTC.deleteMember(log.message.src)
+            break;
+
+            case "ROOM_DATA":
+            webRTC.categorizeData(log.message);
+            break;
+          }
+        }
+      });
+    });
+  }
+
+  updateRoomData() {
+    if(this.room != null) {
+      this.room.getLog();
+    }
+  }
+
+  sendData(inType, inData) {
+    if(this.room == null) {
+      console.log("ルームに入室していません");
+      return;
+    }
+    let data = {
+      type: inType,
+      data: inData
+    };
+    let message = {
+      src: this.peer.id,
+      data: data
+    };
+    if(this.room != null) {
+      this.room.send(data);
+    }
+    this.categorizeData(message);
+  }
+
+  addMember(id) {
+    let data = {
+      type: "msg",
+      data: "入室"
+    };
+    let message = {
+      src: id,
+      data: data
+    };
+    this.categorizeData(message);
+    this.memberList[id] = "exist";
+    this.trigger("member", this.memberList);
+  }
+
+  deleteMember(id) {
+    let data = {
+      type: "msg",
+      data: "退室"
+    };
+    let message = {
+      src: id,
+      data: data
+    };
+    this.categorizeData(message);
+    delete this.memberList[id];
+    this.trigger("member", this.memberList);
+  }
+
+  categorizeData(message) {
+    switch(message.data.type) {
+      case "msg":
+      this.messageList.push({
+        src: message.src,
+        msg: message.data.data
+      });
+      this.trigger("message", this.messageList);
+      break;
+    }
+  }
+
+  trigger(funcName, data = null) {
+    if(typeof this.callback[funcName] === 'function') {
+      this.callback[funcName](data);
+    }
+  }
+
+  on(funcName, func) {
+    if(typeof func === 'function') {
+      this.callback[funcName] = func;
+    }
+  }
+
+  openRoom() {
+    if(this.room != null) {
+      this.trigger("open", this.room);
+    }
+    this.room.getLog();
+  }
+
+  closeRoom() {
+    for(var member in this.memberList){
+      delete this.memberList[member];
+    }
+    this.messageList.splice(0, this.messageList.length);
+    if(this.room != null) {
+      this.trigger("close");
+      this.room.close();
+      this.room = null;
+    }
+  }
+
+  destroyPeer() {
+    this.closeRoom();
+    if(this.peer != null && !this.peer.isDisconnected) {
+      this.trigger("destroy");
+      this.peer.disconnect();
+      this.peer.destroy();
+      this.peer = null;
+    }
+  }
+
+  getPeerID() {
+    if(this.peer != null) {
+      return this.peer.id;
+    }
+    return null;
+  }
+
+  get roomMember() {
+    if(this.room != null) {
+      return this.memberList;
+    }
+    return {};
+  }
+
+  get roomMessage() {
+    if(this.room != null) {
+      return this.messageList;
+    }
+    return new Array(0);
+  }
+}
